e279854bd6b80ab06894e104d273bebbe8bfc81c
SurabhiAngadi <surabhia@sanketika.in>
2023-11-30 10:49:20 +0530

    Issue #50 fix: code refactorig

--- data-products/src/main/scala/org/sunbird/obsrv/dataproducts/job/MasterDataProcessorIndexer.scala
+++ data-products/src/main/scala/org/sunbird/obsrv/dataproducts/job/MasterDataProcessorIndexer.scala
@@ -3,13 +3,13 @@ package org.sunbird.obsrv.dataproducts.job
 import com.redislabs.provider.redis._
 import com.typesafe.config.{Config, ConfigFactory}
 import kong.unirest.Unirest
-import org.apache.spark.sql.{SparkSession}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
 import org.joda.time.{DateTime, DateTimeZone}
 import org.json4s.native.JsonMethods._
 import org.sunbird.obsrv.core.util.JSONUtil
-import org.sunbird.obsrv.dataproducts.helper.{BaseMetricHelper, KafkaMessageProducer}
+import org.sunbird.obsrv.dataproducts.helper.BaseMetricHelper
 import org.sunbird.obsrv.dataproducts.model.{Edata, MetricLabel}
 import org.sunbird.obsrv.model.DatasetModels.{DataSource, Dataset}
 import org.sunbird.obsrv.registry.DatasetRegistry
@@ -24,18 +24,36 @@ object MasterDataProcessorIndexer {
   private val dayPeriodFormat: DateTimeFormatter = DateTimeFormat.forPattern("yyyyMMdd").withZoneUTC()
 
   case class Paths(datasourceRef: String, ingestionPath: String, outputFilePath: String, timestamp: Long)
+
   case class BlobProvider(sparkProviderURIFormat: String, druidProvider: String, druidProviderPrefix: String)
 
   def main(args: Array[String]): Unit = {
     val datasets = DatasetRegistry.getAllDatasets("master-dataset")
     val indexedDatasets = datasets.filter(dataset => {
+      logger.info("Dataset id - " + dataset.id)
       dataset.datasetConfig.indexData.nonEmpty && dataset.datasetConfig.indexData.get
     })
     val metrics = BaseMetricHelper(config)
     indexedDatasets.foreach(dataset => {
       metrics.generate(ets = new DateTime(DateTimeZone.UTC).getMillis, datasetId = dataset.id, edata = Edata(metric = Map(metrics.getMetricName("total_dataset_count") -> 1), labels = List(MetricLabel("job", "MasterDataIndexer"), MetricLabel("datasetId", dataset.id), MetricLabel("cloud", s"${config.getString("cloudStorage.provider")}"))))
-        indexDataset(dataset, metrics, System.currentTimeMillis())
+      indexDataset(dataset, metrics, System.currentTimeMillis())
     })
+  }
+
+  def getSparkSession(dataset: Dataset): (SparkSession, SparkContext) = {
+    try {
+      val conf = new SparkConf()
+        .setAppName("MasterDataProcessorIndexer")
+        .set("spark.redis.host", dataset.datasetConfig.redisDBHost.get)
+        .set("spark.redis.port", String.valueOf(dataset.datasetConfig.redisDBPort.get))
+        .set("spark.redis.db", String.valueOf(dataset.datasetConfig.redisDB.get))
+      val spark = SparkSession.builder().config(conf).getOrCreate()
+      (spark, spark.sparkContext)
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+        throw e
+    }
   }
 
   def indexDataset(dataset: Dataset, metrics: BaseMetricHelper, time: Long): Unit = {
@@ -46,7 +64,8 @@ object MasterDataProcessorIndexer {
       }
       val datasource = datasources.get.head
       val paths = getPaths(datasource)
-      val events_count = createDataFile(dataset, paths.outputFilePath)
+      val (spark, sc) = getSparkSession(dataset)
+      val events_count = createDataFile(dataset, paths.outputFilePath, sc, spark)
       val ingestionSpec = updateIngestionSpec(datasource, paths.datasourceRef, paths.ingestionPath)
       submitIngestionTask(ingestionSpec)
       DatasetRegistry.updateDatasourceRef(datasource, paths.datasourceRef)
@@ -63,15 +82,16 @@ object MasterDataProcessorIndexer {
     }
   }
 
+
   def getPaths(datasource: DataSource): Paths = {
     val dt = new DateTime(DateTimeZone.UTC).withTimeAtStartOfDay()
     val timestamp = dt.getMillis
     val date = dayPeriodFormat.print(dt)
     val provider = providerFormat(config.getString("cloudStorage.provider"))
     val cloudPrefix = if (config.getString("cloudStorage.provider") == "azure") {
-      provider.sparkProviderURIFormat
+      provider.sparkProviderURIFormat + s"""://${config.getString("cloudStorage.accountName")}.blob.core.windows.net/${config.getString("cloudStorage.container")}/"""
     } else {
-      provider.sparkProviderURIFormat
+      provider.sparkProviderURIFormat + s"""://${config.getString("cloudStorage.container")}/"""
     }
     val pathSuffix = s"""masterdata-indexer/${datasource.datasetId}/$date/"""
     val ingestionPath = cloudPrefix.replace(provider.sparkProviderURIFormat, provider.druidProviderPrefix) + pathSuffix
@@ -79,6 +99,7 @@ object MasterDataProcessorIndexer {
     val outputFilePath = cloudPrefix + pathSuffix
     Paths(datasourceRef, ingestionPath, outputFilePath, timestamp)
   }
+
 
   private def updateIngestionSpec(datasource: DataSource, datasourceRef: String, filePath: String): String = {
     val deltaIngestionSpec = deltaIngestionSpecProvider(datasourceRef)
@@ -94,7 +115,8 @@ object MasterDataProcessorIndexer {
     val deltaIngestionSpec = s"""{"type":"index_parallel","spec":{"dataSchema":{"dataSource":"$datasourceRef"},"ioConfig":{"type":"index_parallel"},"tuningConfig":{"type":"index_parallel","maxRowsInMemory":500000,"forceExtendableShardSpecs":false,"logParseExceptions":true}}}"""
     deltaIngestionSpec
   }
-  private def inputSourceSpecProvider(filePath: String): String ={
+
+  private def inputSourceSpecProvider(filePath: String): String = {
     val provider = providerFormat(config.getString("cloudStorage.provider"))
     val inputSourceSpec = if (provider.druidProvider == "local")
       s"""{"spec":{"ioConfig":{"type":"index_parallel","inputSource":{"type":"${provider.druidProvider}","baseDir":"${config.getString("cloudStorage.container")}","filter":"**.json.gz"}}}}"""
@@ -104,12 +126,12 @@ object MasterDataProcessorIndexer {
 
   private def providerFormat(cloudProvider: String): BlobProvider = {
     cloudProvider match {
-      case "local" => BlobProvider(s"file://${config.getString("cloudStorage.container")}/", "local", "file")
-      case "aws" => BlobProvider(s"s3a://${config.getString("cloudStorage.container")}/","s3", "s3")
-      case "azure" => BlobProvider(s"wasbs://${config.getString("cloudStorage.accountName")}.blob.core.windows.net/azure/", "azure", "azure")
-      case "gcloud" => BlobProvider(s"gs://${config.getString("cloudStorage.container")}/", "google", "gs")
-      case "cephs3" => BlobProvider(s"s3a://${config.getString("cloudStorage.container")}/", "s3", "s3") // TODO: Have to check Druid compatibility
-      case "oci" => BlobProvider(s"s3a://${config.getString("cloudStorage.container")}/", "s3", "s3") // TODO: Have to check Druid compatibility
+      case "local" => BlobProvider("file", "local", "file")
+      case "aws" => BlobProvider("s3a", "s3", "s3")
+      case "azure" => BlobProvider("wasbs", "azure", "azure")
+      case "gcloud" => BlobProvider("gs", "google", "gs")
+      case "cephs3" => BlobProvider("s3a", "s3", "s3") // TODO: Have to check Druid compatibility
+      case "oci" => BlobProvider("s3a", "s3", "s3") // TODO: Have to check Druid compatibility
       case _ => throw new Exception("Unsupported provider")
     }
   }
@@ -119,45 +141,34 @@ object MasterDataProcessorIndexer {
       .header("Content-Type", "application/json")
       .body(ingestionSpec).asJson()
     logger.info("Ingestion spec - " + response.getBody)
-    response.ifFailure(response => throw new Exception(s"Exception while submitting ingestion task - ${response.getBody}"))
+    response.ifFailure(response => throw new Exception(s"Exception while submitting ingestion task - ${response.getBody}, with status - ${response.getStatus}"))
   }
 
   private def deleteDataSource(datasourceRef: String): Unit = {
     val response = Unirest.delete(config.getString("druid.datasource.delete.url") + datasourceRef)
       .header("Content-Type", "application/json")
       .asJson()
-    response.ifFailure(response => throw new Exception("Exception while deleting datasource" + datasourceRef + " ,Response body - " + response.getBody))
+    response.ifFailure(response => throw new Exception(s"Exception while deleting datasource ${datasourceRef} - ${response.getBody}, with status - ${response.getStatus}"))
   }
 
-  def createDataFile(dataset: Dataset,outputFilePath: String) = {
-    val conf = new SparkConf()
-      .setAppName("MasterDataProcessorIndexer")
-      .set("spark.redis.host", dataset.datasetConfig.redisDBHost.get)
-      .set("spark.redis.port", String.valueOf(dataset.datasetConfig.redisDBPort.get))
-      .set("spark.redis.db", String.valueOf(dataset.datasetConfig.redisDB.get))
+  def createDataFile(dataset: Dataset, outputFilePath: String, sc: SparkContext, spark: SparkSession): Long = {
     val readWriteConf = ReadWriteConfig(scanCount = config.getInt("redis_scan_count"), maxPipelineSize = config.getInt("redis_maxPipelineSize"))
-    val sc = new SparkContext(conf)
-    val spark = new SparkSession.Builder().config(conf).getOrCreate()
-    val rdd = sc.fromRedisKV("*")(readWriteConfig = readWriteConf)
-      .map(f =>
-        processEvent(f._2)
-      )
-    val response = rdd.collect()
-    val stringifiedResponse = JSONUtil.serialize(response)
-    val df = spark.read.json(spark.sparkContext.parallelize(Seq(stringifiedResponse)))
+    val ts = new DateTime(DateTimeZone.UTC).withTimeAtStartOfDay().getMillis
+    val rdd = sc.fromRedisKV("*")(readWriteConfig = readWriteConf).map(
+      f => processEvent(f._2, ts)
+    )
+    val df = spark.read.json(rdd)
     val noOfRecords = df.count()
-    println("Dataset - " + dataset.id + " No. of records - " + noOfRecords)
+    logger.info("Dataset - " + dataset.id + " No. of records - " + noOfRecords)
     df.coalesce(1).write.mode("overwrite").option("compression", "gzip").json(outputFilePath)
-    spark.stop()
     sc.stop()
+    spark.stop()
     noOfRecords
   }
 
-  private def processEvent(value: String) = {
-    val dt = new DateTime(DateTimeZone.UTC).withTimeAtStartOfDay()
-    val timestamp = dt.getMillis
+  private def processEvent(value: String, ts: Long) = {
     val json = JSONUtil.deserialize[mutable.Map[String, AnyRef]](value)
-    json("obsrv_meta") = mutable.Map[String, AnyRef]("syncts" -> timestamp.asInstanceOf[AnyRef]).asInstanceOf[AnyRef]
-    json
+    json("obsrv_meta") = mutable.Map[String, AnyRef]("syncts" -> ts.asInstanceOf[AnyRef]).asInstanceOf[AnyRef]
+    JSONUtil.serialize(json)
   }
 }
